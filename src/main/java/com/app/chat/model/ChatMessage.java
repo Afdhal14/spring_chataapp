62b7ca0cda1b31f048bb11b6d47e70331a4afe0a
Afdhal14 <afdhalofficial1414@gmail.com>
2025-06-25 18:23:49 +0530

    Added few functions in HTML to communicate in real time

--- src/main/java/com/app/chat/model/ChatMessage.java
+++ src/main/java/com/app/chat/model/ChatMessage.java
@@ -6,7 +6,37 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class ChatMessage {
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
     private long id;
     private String sender;
     private String content;
+
+    public ChatMessage(String content, String sender, long id) {
+        this.content = content;
+        this.sender = sender;
+        this.id = id;
+    }
 }
