8e5d0cc1dc1d0fc9da33b684fa31ce033b7677c8
Afdhal14 <afdhalofficial1414@gmail.com>
2025-07-04 12:32:30 +0530

    Added user login with database integration

--- src/main/java/com/app/chat/configuration/SecurityConfiguration.java
+++ src/main/java/com/app/chat/configuration/SecurityConfiguration.java
@@ -1,16 +1,24 @@
 package com.app.chat.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
 
+    @Autowired
+    private UserDetailsService userDetailService;
 
     //Here we are saying to spring boot that don't go for the default filter chain go for the filter chain I give you
     @Bean
@@ -18,8 +26,19 @@ public class SecurityConfiguration {
 
         http.csrf(customizer->customizer.disable());
         http.authorizeHttpRequests(request->request.anyRequest().authenticated());
-        http.formLogin(Customizer.withDefaults());
+      //http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());//This is for REST calls
+        http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider()
+    {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        provider.setUserDetailsService(userDetailService);
+        return provider;
     }
 
 }
